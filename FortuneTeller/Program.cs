a6cb355dd5a0ef8e6c01fa5db1f7cfaf2206e351
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-05-27 03:52:14 -0500

    2nd draft

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -6,16 +6,16 @@ namespace NextBirthday
     {
         public static void Main(string[] args)
         {
-            NextBirthday();
-        }
-        // TODO: Create a method that takes the date someone was born and returns the name of the day on their next birthday.
-        public static void NextBirthday()
-        {
             Console.WriteLine("When is your birthday?");
             DateTime myBirthday = DateTime.Parse(Console.ReadLine());
+            NextBirthdayDay(myBirthday);
+        }
+        // TODO: Create a method that takes the date someone was born and returns the name of the day on their next birthday.
+        public static string NextBirthdayDay(DateTime myBirthday)
+        {
             Console.WriteLine("Your birthday will be on a {0} this year and on a {1} next year", 
                 myBirthday.DayOfWeek, myBirthday.AddYears(1).DayOfWeek);
-            Console.ReadLine();
+            return Console.ReadLine();
         }
     }
 }
